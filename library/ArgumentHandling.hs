eb5127f8792ba844eddaa110f14a7367f71f6690
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-03-30 06:47:23 +0200

    3 days to fucking do a simple function

--- library/ArgumentHandling.hs
+++ library/ArgumentHandling.hs
@@ -9,20 +9,31 @@ parse :: [String] -> IO ()
 parse ["-h"]        = usage >> exit
 parse ["--help"]    = usage >> exit
 parse []            = die84
-parse args          =
-    if not (isValidExpression args)
-        then die84
-        else return()
+parse args
+    | not (isSingleArg args) = die84
+    | not (isValidExpression (head args)) = die84
+    | otherwise = return()
 
 usage :: IO ()
 usage   = putStrLn "Usage: ./funEvalExpr math_expression"
+
 exit :: IO ()
 exit    = exitSuccess
+
 die84 :: IO ()
 die84   = exitWith (ExitFailure 84)
 
-isValidExpression :: [String] -> Bool
-isValidExpression args =
-    if (length args == 1)
-        then True
-        else False
+isSingleArg :: [String] -> Bool
+isSingleArg args = length args == 1
+
+isValidExpression :: String -> Bool
+isValidExpression expression = lengthCompare expression (expressionFiltered expression) == EQ
+
+expressionFiltered :: String -> String
+expressionFiltered expression = filter (`elem` authorizedCharacters) expression
+
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = (length x `compare` length y) `mappend` (x `compare` y)
+
+authorizedCharacters :: String
+authorizedCharacters = "0123456789 ().+-/*%"

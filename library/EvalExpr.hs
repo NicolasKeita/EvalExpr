2aa35bfae0c873b407e16309a656f50e1f0619bc
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-04-05 13:42:00 +0200

    add: space between operators

--- library/EvalExpr.hs
+++ library/EvalExpr.hs
@@ -1,6 +1,5 @@
 module EvalExpr (main) where
 import Control.Applicative ((<$>), (<*>))
-import System.Environment
 import System.Exit
 import Data.Fixed
 import Text.Printf
@@ -11,12 +10,41 @@ round6dp x = fromIntegral (round $ x * 1e2) / 1e2
 die84 :: IO ()
 die84   = exitWith (ExitFailure 84)
 
+replace :: Eq a => [a] -> [a] -> [a] -> [a]
+replace needle replacement haystack
+  = case begins haystack needle of
+      Just remains -> replacement ++ remains
+      Nothing      -> case haystack of
+                        []     -> []
+                        x : xs -> x : replace needle replacement xs
+
+begins :: Eq a => [a] -> [a] -> Maybe [a]
+begins haystack []                = Just haystack
+begins (x : xs) (y : ys) | x == y = begins xs ys
+begins _        _                 = Nothing
+
+modifExpression :: String -> String
+modifExpression expression = modifExpressionMod (modifExpressionDiv (modifExpressionSub (modifExpressionMul (modifExpressionAdd expression))))
+
+modifExpressionAdd :: String -> String
+modifExpressionAdd = replace "+" " + "
+
+modifExpressionMul :: String -> String
+modifExpressionMul = replace "*" " * "
+
+modifExpressionSub :: String -> String
+modifExpressionSub = replace "-" " - "
+
+modifExpressionDiv :: String -> String
+modifExpressionDiv = replace "/" " / "
+
+modifExpressionMod :: String -> String
+modifExpressionMod = replace "%" " % "
+
 main :: String -> IO ()
-main expression = case calculate expression of
---main expression = printf "%.2f" (case calculate expression of
+main expression = case calculate (modifExpression expression) of
     Just n -> printf "%.2f" (round6dp n) 
     Nothing -> die84
---main expression = print (calculate expression)
 
 type Operator = Double -> Double -> Double
 type Entry = (String, Operator)
